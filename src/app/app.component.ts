b489fda9a49c3d5d451e03d52661b3a0500f2eb8
Alvaro Condori <condorialvaro95@gmail.com>
2024-10-18 16:12:53 -0300

    implementando estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  welcome = 'Hola!';
-  tasks = ['Instalar el Angular CLI', 'Crear proyecto', 'Crear componentes'];
+  title = 'todo-app';
 }
