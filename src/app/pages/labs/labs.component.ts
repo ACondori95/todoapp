5d7aa4ba771b50ad79dd277231b670bd8ae5227a
Alvaro Condori <condorialvaro95@gmail.com>
2024-10-20 23:19:54 -0300

    event handling: click y doble click

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -26,4 +26,12 @@ export class LabsComponent {
     age: 29,
     avatar: 'https://w3schools.com/howto/img_avatar.png',
   };
+
+  clickHandler() {
+    alert('Hola');
+  }
+
+  changeHandler(event: Event) {
+    console.log(event);
+  }
 }
